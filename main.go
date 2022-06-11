c4f9405415d79537f7b2944b8781139cc6fde881
padhitheboss <padhithebossno.1@gmail.com>
2022-06-11 19:48:37 +0530

    Handler Added

--- main.go
+++ main.go
@@ -7,10 +7,28 @@ import (
 )
 
 func formhandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.URL.Path != "/form" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Post Request Successful")
+	name := r.FormValue("name")
+	email := r.FormValue("email")
+	address := r.FormValue("address")
+	fmt.Fprintf(w, "Name =%s\n", name)
+	fmt.Fprintf(w, "Email=%s/n", email)
+	fmt.Fprintf(w, "Address=%s/n", address)
 }
 func hellohandler(w http.ResponseWriter, r *http.Request) {
-
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 Not Found", http.StatusNotFound)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method Not Supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(w, "Hello!")
 }
 func main() {
 	fileserver := http.FileServer(http.Dir("./static"))
