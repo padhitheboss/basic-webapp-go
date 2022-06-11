7667b6c81badaf12e6eb76c15896959a0962c62e
padhitheboss <padhithebossno.1@gmail.com>
2022-06-11 19:23:05 +0530

    Basic Structure Added

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func formhandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func hellohandler(w http.ResponseWriter, r *http.Request) {
+
+}
+func main() {
+	fileserver := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileserver)
+	http.HandleFunc("/hello", hellohandler)
+	http.HandleFunc("/form", formhandler)
+	fmt.Println("Server Started at Port 3000")
+	if err := http.ListenAndServe("3000", nil); err != nil {
+		log.Fatal(err)
+	}
 }
